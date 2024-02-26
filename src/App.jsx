ecbcfd9ddef9e66e278ca28aa22d4cf1b763715a
Sandersaether <sander.sather@gmail.com>
2024-02-26 14:52:14 +0100

    core and extension1

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,119 @@
-import "./App.css"
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  const [operation, setOperation] = useState('');
+  const [answer, setAnswer] = useState('');
+
+  const handleNumberClick2 = (num) => {
+    if (number2 === '0') {
+      setNumber2(num.toString());
+    } else {
+      setNumber2((prevNumber) => prevNumber + num);
+    }
+    }
+
+  const handleNumberClick = (num) => {
+    if (number1 === '0') {
+      setNumber1(num.toString());
+    } else {
+      setNumber1((prevNumber) => prevNumber + num);
+    }
+  };
+
+  const handleBackArrowClick = () => {
+      setNumber1((prevNumber) => prevNumber.slice(0, -1));   
+  };
+  const handleBackArrowClick2 = () => {
+    setNumber2((prevNumber) => prevNumber.slice(0, -1));
+  };
+
+  const handleOperationClick = (op) => {
+    setOperation(op);
+  };
+
+  const handleClearClick = () => {
+    setNumber1('');
+    setNumber2('');
+    setOperation('');
+    setAnswer('');
+  };
+
+  const handleCalculate = () => {
+    const num1 = parseFloat(number1);
+    const num2 = parseFloat(number2);
+
+    if (!isNaN(num1) && !isNaN(num2) && operation) {
+      switch (operation) {
+        case '+':
+          setAnswer(num1 + num2);
+          break;
+        case '-':
+          setAnswer(num1 - num2);
+          break;
+        case '*':
+          setAnswer(num1 * num2);
+          break;
+        case '/':
+          setAnswer(num1 / num2);
+          break;
+        default:
+          setAnswer('Invalid operation');
+      }
+    } else {
+      setAnswer('Invalid input');
+    }
+  };
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{number1}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
+            <button key={num} onClick={() => handleNumberClick(num)}>
+              {num}
+            </button>
+          ))}
+          <button onClick={handleBackArrowClick}>&larr;</button>
+          <button onClick={handleClearClick}>Clear</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{operation}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          {['+', '-', '*', '/'].map((op) => (
+            <button key={op} onClick={() => handleOperationClick(op)}>
+              {op}
+            </button>
+          ))}
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{number2}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
+            <button key={num} onClick={() => handleNumberClick2(num)}>
+              {num}
+            </button>
+          ))}
+          <button onClick={handleBackArrowClick2}>&larr;</button>
+          <button onClick={handleClearClick}>Clear</button>
         </div>
       </div>
+
       <div className="panel answer">
-        <p>0</p>
+        <p>{answer}</p>
         <div>
-          <button>=</button>
+          <button onClick={handleCalculate}>=</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
